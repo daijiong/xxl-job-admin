f2fc13f9c79761de271f01386643a5ae9c878aa3
daijiong <daijiong@successhr.com>
2022-07-06 23:24:28 +0800

    去除spring-boot-admin

--- src/main/java/com/xxl/job/admin/core/conf/MetricsConfig.java
+++ src/main/java/com/xxl/job/admin/core/conf/MetricsConfig.java
@@ -1,20 +1,20 @@
-package com.xxl.job.admin.core.conf;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
-import org.springframework.context.annotation.Bean;
-
-import io.micrometer.core.instrument.MeterRegistry;
-
-/**
- * Description
- *
- * @author daijiong
- * @version : MetricsConfig.java, v 0.1 2021/3/25 12:18 daijiong Exp $$
- */
-public class MetricsConfig {
-    @Bean
-    MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
-        return (registry) -> registry.config().commonTags("application", applicationName);
-    }
-}
+//package com.xxl.job.admin.core.conf;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+//import org.springframework.context.annotation.Bean;
+//
+//import io.micrometer.core.instrument.MeterRegistry;
+//
+///**
+// * Description
+// *
+// * @author daijiong
+// * @version : MetricsConfig.java, v 0.1 2021/3/25 12:18 daijiong Exp $$
+// */
+//public class MetricsConfig {
+//    @Bean
+//    MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+//        return (registry) -> registry.config().commonTags("application", applicationName);
+//    }
+//}
